b92df083aed4cb471514fdfe0e11d654b75bf481
afope <fope.ojo@gmail.com>
2017-01-18 20:07:05 +0100

    Adding the files

---
+++ app.js
@@ -0,0 +1,22 @@
+var app = angular.module('Todo', []);
+app.controller('TodoCtrl', function($scope) {
+  $scope.message = 'Angular is pretty cool.';
+  $scope.todos = [
+    'Learn Sketch',
+    'Look at Dribble and feel inferior',
+    'Actually learn to use pen tool'
+  ];
+  $scope.done = function(todo) {
+    var indexOf = $scope.todos.indexOf(todo);
+    if (indexOf !== -1) {
+      $scope.todos.splice(indexOf, 1);
+    }
+  };
+  $scope.add = function(e) {
+    if (e.which && e.which === 13) {
+      $scope.todos.push($scope.newTodo);
+      $scope.newTodo = '';
+    }
+  };
+
+});
